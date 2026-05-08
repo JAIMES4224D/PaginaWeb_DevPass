5af78a25fb9a76c433f5547f640793219c030ba2
Jeferson Jaimes Passuni <93844155+JAIMES4224D@users.noreply.github.com>
2026-05-08 13:34:57 -0500

    Configure site and base URL in astro.config.mjs
    Set site and base URL for GitHub Pages deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'; // La configuración que usamos antes
 
 export default defineConfig({
+  site: 'https://JAIMES4224D.github.io',
+  base: 'PaginaWeb_DevPass', // IMPORTANTE: Nombre exacto de tu repo en GitHub
   vite: {
     plugins: [tailwindcss()],
   },
