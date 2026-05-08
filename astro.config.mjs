d0625d15a1c0a2190b39f35dd1e3d29f867ba94d
Jeferson Jaimes Passuni <93844155+JAIMES4224D@users.noreply.github.com>
2026-05-08 13:58:39 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite'; 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  site: 'https://JAIMES4224D.github.io',
-  base: 'PaginaWeb_DevPass', 
   vite: {
     plugins: [tailwindcss()],
   },
+  // otras configuraciones...
 });
